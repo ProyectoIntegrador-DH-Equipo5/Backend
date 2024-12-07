5635fe57512747a01a6fda591bd0cb7dfce6d712
lizethalonso <lizethalsa23@gmail.com>
2024-12-06 22:44:24 -0500

    Agregando dominio CORS

--- src/main/java/com/artxp/artxp/security/cors/corsConfig.java
+++ src/main/java/com/artxp/artxp/security/cors/corsConfig.java
@@ -10,7 +10,7 @@ public class corsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite el mapeo CORS para todas las rutas.
-                .allowedOrigins("http://localhost:5173") // Permite solo esta URL de origen.
+                .allowedOrigins("http://localhost:5173", "https://frontend-murex-one-19.vercel.app") // Permite solo esta URL de origen.
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite solo estos métodos HTTP.
                 .allowedHeaders("Authorization",
                         "Accept",
