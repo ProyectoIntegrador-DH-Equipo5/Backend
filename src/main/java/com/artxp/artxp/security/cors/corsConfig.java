9393107b744b0a49bf6e19bd527ab2d3507dfb0d
lizethalonso <lizethalsa23@gmail.com>
2024-12-11 19:41:57 -0500

    Agreg nueva dirección front

--- src/main/java/com/artxp/artxp/security/cors/corsConfig.java
+++ src/main/java/com/artxp/artxp/security/cors/corsConfig.java
@@ -10,8 +10,7 @@ public class corsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite el mapeo CORS para todas las rutas.
-                .allowedOrigins("http://localhost:5173", "https://frontend-murex-one-19.vercel.app",
-                        "https://frontend-git-respaldoconexion2-artxperiences-projects.vercel.app/") // Permite solo esta URL de origen.
+                .allowedOrigins("http://localhost:5173", "https://frontend-5-eta.vercel.app/") // Permite solo esta URL de origen.
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite solo estos métodos HTTP.
                 .allowedHeaders("Authorization",
                         "Accept",
