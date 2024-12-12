4962d3593f51de1ad5f55f713ec8c886f8e156e7
lizethalonso <lizethalsa23@gmail.com>
2024-12-11 23:10:40 -0500

    Ensayo en cors

--- src/main/java/com/artxp/artxp/security/cors/corsConfig.java
+++ src/main/java/com/artxp/artxp/security/cors/corsConfig.java
@@ -10,7 +10,7 @@ public class corsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite el mapeo CORS para todas las rutas.
-                .allowedOrigins("http://localhost:5173", "https://frontend-5-eta.vercel.app/") // Permite solo esta URL de origen.
+                .allowedOrigins("http://localhost:5173", "https://frontend-5-eta.vercel.app") // Permite solo esta URL de origen.
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite solo estos métodos HTTP.
                 .allowedHeaders("Authorization",
                         "Accept",
